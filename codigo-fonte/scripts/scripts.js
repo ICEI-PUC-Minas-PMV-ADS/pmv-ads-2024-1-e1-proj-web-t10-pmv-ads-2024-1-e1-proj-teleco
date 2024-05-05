a29b10105c648f9ada93d8c519c90442d785acc7
Guilherme Silva <73372109+guilherme1991g@users.noreply.github.com>
2024-05-05 20:02:17 -0300

    organizado

---
+++ codigo-fonte/scripts/scripts.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyAx7QEvYUplmxg7pk6I3Bd99F5y-F1MoO8",
+  authDomain: "triton-331e9.firebaseapp.com",
+  projectId: "triton-331e9",
+  storageBucket: "triton-331e9.appspot.com",
+  messagingSenderId: "915939586386",
+  appId: "1:915939586386:web:4fd37cee5bfa51f2614a8b",
+  measurementId: "G-KQ8MY42QT1"
+};
