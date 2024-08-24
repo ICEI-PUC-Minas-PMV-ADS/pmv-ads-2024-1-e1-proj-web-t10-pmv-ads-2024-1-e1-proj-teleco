9776c86aac0e12c7f6d3b64aeaa3f9feffc4fd89
Guilherme Silva <73372109+guilherme1991g@users.noreply.github.com>
2024-08-24 05:56:34 -0300

    teste
    teste

--- codigo-fonte/scripts/relatorio-vendas.js
+++ codigo-fonte/scripts/relatorio-vendas.js
@@ -1,66 +1,89 @@
-pegadados()
+const url = "http://ap3.stc.srv.br/integration/prod/ws/vehicle/list"; // API URL
+const method = "POST"; // Request method, change for what's needed
 
-function pegadados() {
-    arrayvenda = JSON.parse(localStorage.getItem('arrayvenda'))
-    var soma = 0
-    var count = arrayvenda.length
-    var tabela = document.getElementById('tabela')
+const update = {
+    key: '7e0a6b6b42558761470d6269804fa428'
+};
 
+fetch(url, {
+    method,
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(update),
 
-    for (i = count; i > 0; i--) {
-        let tr = tabela.insertRow()
-        let tddata = tr.insertCell()
-        let tdvalor = tr.insertCell()
-        let tdproduto = tr.insertCell()
-        let tduser = tr.insertCell()
-
-
-        tddata.innerText = arrayvenda[i - 1].data
-        tdvalor.innerText = arrayvenda[i - 1].vtotal
-        tdvalor.setAttribute("class", "valor")
-        tdproduto.innerText = arrayvenda[i - 1].produto
-        tduser.innerText = arrayvenda[i - 1].user
-
+}).then((response) => {
+    if (!response.ok) {
+        throw new Error("Network response was not ok");
     }
+    return response.json();
+})
+    .then((data) => {
 
-    for (i = count; i > 0; i--) {
-        soma = soma + parseFloat(arrayvenda[i - 1].vtotal)
-    }
-    var resultado = document.getElementById('resultado')
-    resultado.innerText = parseFloat(soma).toFixed(2)
+        for (i=5 ; i>0 ; i--){
+
+        var placa = data.data.data[i].lisencePlate
+        getSga(placa)
+        }
+    })
+    .catch((error) => console.error("Fetch error:", error));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function resposta(resposta) {
+    let tr = tabela.insertRow()
+    let tddata = tr.insertCell()
+    let tdvalor = tr.insertCell()
+    let tdproduto = tr.insertCell()
+    let tduser = tr.insertCell()
+
+
+    tddata.innerText = resposta
+
 }
 
-function filtradados() {
+function getSga(placa) {
+    const url = `https://api.hinova.com.br/api/sga/v2/veiculo/buscar/${placa}/placa`; // API URL
+    const token = "4962a431d48bdc7d85fbe39d430f8421191f6f6c9fe4593529cd9295177dc37c8e7f2a2e7d5bd7ab42258b1752a624c857cfa950a931fc1b822fd6885c847a2265c80bab40556abd4b8c782e08d8450108f990417ef0f723e7c3998def8b61668fdda80c0bd818be85f295a3641d8bf85857f054461c0992d50c8f4dc48eb149cbe5f5690d95d69d9c05fe92864c2ed4"; // API Token
+    const method = "GET"; // Request method, change for what's needed
 
-    const busca = document.getElementById('busca');
-    const tabela = document.getElementById('tabela');
+    fetch(url, {
+        method,
+        headers: {
 
-    let expressao = busca.value.toLowerCase();
-    var valorcalculado = 0
-    let linhas = tabela.getElementsByTagName('tr');
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"	 // This is the important part, the auth header
+        },
+        // body: JSON.stringify(update),
 
-
-    for (let posicao in linhas) {
-        if (true === isNaN(posicao)) {
-            continue;
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error('Placa não encontrada: ' + placa);
         }
-
-        let conteudoDaLinha = linhas[posicao].innerHTML.toLowerCase();
-
-        if (true === conteudoDaLinha.includes(expressao)) {
-            linhas[posicao].style.display = ''
-            var valor = linhas[posicao].innerHTML
-            valor = valor.split('class="valor">')[1]
-            valor = valor.split('<')[0]
-            valorcalculado = parseFloat(valor) +valorcalculado
-            var resultado = document.getElementById('resultado')
-            resultado.innerText = valorcalculado.toFixed(2)           
-
-        } else {
-            linhas[posicao].style.display = 'none';
-        }
-    }
-
-
-
+        return response.json();
+    })
+        .then((data) => {
+            resposta(data[0].descricao_situacao)
+        })
+        .catch((error) => {
+            resposta(error)
+        });
 }
+
+
+
